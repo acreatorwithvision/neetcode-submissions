5de96c9d9ebd3050d7703f5aa8f248da56a63113
Suhas G <154875420+acreatorwithvision@users.noreply.github.com>
2026-06-09 14:03:46 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+
+        mapping={
+            ")":"(",
+            "}":"{",
+            "]":"["
+        }
+
+        for ch in s:
+            if ch in mapping:
+                if not stack:
+                    return False
+
+                top=stack.pop()
+
+                if top!=mapping[ch]:
+                    return False
+            else:
+                stack.append(ch)
+        return not stack
